93d95842339336ae0b4bbfc15b487a57853be35a
bluef <bluef@NOTLASTNOVA>
2023-04-27 23:57:25 +0500

    Добавьте файлы проекта.

---
+++ WpfApp3/SortingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3 {
+        public class SortingItem
+        {
+            public string DisplayText { get; set; }
+            public SortDescription Description { get; set; }
+        }
+    }
+
